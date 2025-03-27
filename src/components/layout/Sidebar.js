83cc04ed8c6017baa33ae188f9d804d76f71635c
d4rkx666 <fcatzin@hotmail.com>
2025-03-27 14:59:05 -0500

    Hide menu from prod
    hidding on-work menu

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -9,9 +9,7 @@ function Sidebar() {
     <aside className="hidden lg:flex w-64 p-5 border-r border-blue-100">
       <nav className="flex flex-col gap-2">
         <NavLink to="/dashboard" className={({ isActive }) => `text-sm ${isActive ? "text-blue-600 font-bold" : "text-gray-400 hover:text-gray-500"}`}>{labels.menu.dashboard}</NavLink>
-        <NavLink to="/profile" className={({ isActive }) => `text-sm ${isActive ? "text-blue-600 font-bold" : "text-gray-400 hover:text-gray-500"}`}>{labels.menu.profile}</NavLink>
         <NavLink to="/resume" className={({ isActive }) => `text-sm ${isActive ? "text-blue-600 font-bold" : "text-gray-400 hover:text-gray-500"}`}>{labels.menu.resume}</NavLink>
-        <NavLink to="/create-resume" className={({ isActive }) => `text-sm ${isActive ? "text-blue-600 font-bold" : "text-gray-400 hover:text-gray-500"}`}>{labels.menu.createResume}</NavLink>
       </nav>
     </aside>
   );
