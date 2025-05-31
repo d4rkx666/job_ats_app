61c82cccecd10365e1f9f4180b534e513965404b
d4rkx666 <fcatzin@hotmail.com>
2025-05-30 20:47:47 -0500

    Nuevo LOGO

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -33,7 +33,7 @@ function Header() {
         {/* App Name */}
         <div className="text-2xl font-bold">
           <NavLink to="/" className="hidden md:flex text-blue-700 no-underline hover:text-blue-300 transition duration-300">
-            {config.appName}
+            <span className="name1">{config.appName.split(" ")[0]}</span><span className="name2">{config.appName.split(" ")[1]}</span>
           </NavLink>
           <NavLink to="/" className="md:hidden text-blue-700 no-underline hover:text-blue-300 transition duration-300">
             <img src="/logo.svg" alt="logo" className="w-10"/>
